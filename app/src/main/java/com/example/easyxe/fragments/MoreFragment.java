0173f167938a46bf4bab869d5a02a28218eac921
Chi <vo.chi.210@gmail.com>
2019-05-07 19:42:29 +0700

    Screen 10

--- app/src/main/java/com/example/easyxe/fragments/MoreFragment.java
+++ app/src/main/java/com/example/easyxe/fragments/MoreFragment.java
@@ -7,8 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
 import com.example.easyxe.R;
+import com.example.easyxe.adapters.CustomExpandableListView;
+import com.example.easyxe.adapters.ExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -16,10 +23,15 @@ import com.example.easyxe.R;
  * Activities that contain this fragment must implement the
  * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link MoreFragment#newInstance} factory method to
+ * Use the {@link NotifFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class MoreFragment extends Fragment {
+    ExpandableListView listView;
+    List<String> listdataHeader;
+    HashMap<String, List<String>> listHash;
+    CustomExpandableListView listAdapter;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -41,11 +53,11 @@ public class MoreFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MoreFragment.
+     * @return A new instance of fragment NotifFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MoreFragment newInstance(String param1, String param2) {
-        MoreFragment fragment = new MoreFragment();
+    public static NotifFragment newInstance(String param1, String param2) {
+        NotifFragment fragment = new NotifFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,7 +78,38 @@ public class MoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+        listView = (ExpandableListView)view.findViewById(R.id.expandableListView);
+        initData();
+
+
+        listAdapter = new CustomExpandableListView(getActivity(), listdataHeader, listHash);
+        listView.setAdapter(listAdapter);
+        return view;
+
+
+    }
+
+    private void initData(){
+        listdataHeader = new ArrayList<>();
+        listHash = new HashMap<String, List<String>>();
+
+        listdataHeader.add("Phiên bản");
+        listdataHeader.add("Liên hệ");
+        listdataHeader.add("Báo cáo sự cố");
+
+        List<String> phienban = new ArrayList<String>();
+        phienban.add("v.0.1");
+
+        List<String> lienhe = new ArrayList<String>();
+        lienhe.add("Phone Number");
+        lienhe.add("Messenger");
+
+        List<String> baocao = new ArrayList<String>();
+
+        listHash.put(listdataHeader.get(0), phienban);
+        listHash.put(listdataHeader.get(1), lienhe);
+        listHash.put(listdataHeader.get(2), baocao);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
